920689d8769a50ddf12a085ed9174ef80184b7e9
Sean Johnson <sean@snootymonkey.com>
2017-01-21 05:25:40 -0500

    Add unique ID checker to schema.

--- src/oc/lib/schema.clj
+++ src/oc/lib/schema.clj
@@ -2,13 +2,15 @@
   "Prismatic schema common data schema fragments."
   (:require [clojure.string :as s]
             [schema.core :as schema]))
-  
+
+(defn unique-id? [s]
+  (if (and s (re-matches #"^(\d|[a-f]){4}-(\d|[a-f]){4}-(\d|[a-f]){4}$" s)) true false))
 
 (def NonBlankString (schema/pred #(and (string? %) (not (s/blank? %)))))
 
 ;; 12 character fragment from a UUID e.g. 51ab-4c86-a474
 (def UniqueID (schema/pred #(and (string? %)
-                                 (re-matches #"^(\d|[a-f]){4}-(\d|[a-f]){4}-(\d|[a-f]){4}$" %)))) 
+                                 (unique-id? %))))
 
 (def ISO8601 (schema/pred #(and (string? %)
                                 (re-matches #"(?i)^\d{4}-\d\d-\d\dT\d\d:\d\d:\d\d(\.\d+)?(([+-]\d\d:\d\d)|Z)?$" %))))

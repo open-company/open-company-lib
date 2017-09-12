cc9630820cd3c7f348a41d34b7209d1228187205
Sean Johnson <sean@snootymonkey.com>
2017-09-12 17:32:23 -0400

    New oc.lib release.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject open-company/lib "0.12.16"
+(defproject open-company/lib "0.12.17"
   :description "OpenCompany Common Library"
   :url "https://github.com/open-company/open-company-lib"
   :license {

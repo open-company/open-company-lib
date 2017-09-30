42653b8f87f5901f01335398dc14b87a8a7878b0
Sean Johnson <sean@snootymonkey.com>
2017-09-30 07:18:49 -0400

    Working out of some version dependency hell by making oc.lib have no conflicts that lein has to solve itself. Exclusions FTW!

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject open-company/lib "0.13.3"
+(defproject open-company/lib "0.14.0"
   :description "OpenCompany Common Library"
   :url "https://github.com/open-company/open-company-lib"
   :license {
@@ -13,30 +13,75 @@
 
   ;; All profile dependencies
   :dependencies [
-    [org.clojure/clojure "1.9.0-beta1" :scope "provided"] ; Lisp on the JVM http://clojure.org/documentation
-    [org.clojure/core.async "0.3.443"] ; Async programming and communication https://github.com/clojure/core.async
-    [org.clojure/core.match "0.3.0-alpha5"] ; Erlang-esque pattern matching https://github.com/clojure/core.match
-    [defun "0.3.0-RC1"] ; Erlang-esque pattern matching for Clojure functions https://github.com/killme2008/defun
-    [lockedon/if-let "0.1.0"] ; More than one binding for if/when macros https://github.com/LockedOn/if-let
-    [com.stuartsierra/component "0.3.2"] ; Component Lifecycle https://github.com/stuartsierra/component
+    ;; Lisp on the JVM http://clojure.org/documentation
+    [org.clojure/clojure "1.9.0-beta1" :scope "provided"]
+    ;; Async programming and communication https://github.com/clojure/core.async
+    [org.clojure/core.async "0.3.443"]
+    ;; Erlang-esque pattern matching https://github.com/clojure/core.match
+    [org.clojure/core.match "0.3.0-alpha5"]
+    ;; Clojure reader https://github.com/clojure/tools.reader
+    ;; NB: Not used directly, but a very common dependency, so pulled in for manual version management
+    [org.clojure/tools.reader "1.1.0"]
+    ;; Tools for writing macros https://github.com/clojure/tools.macro
+    ;; NB: Not used directly, but a very common dependency, so pulled in for manual version management
+    [org.clojure/tools.macro "0.1.5"]
+    ;; Erlang-esque pattern matching for Clojure functions https://github.com/killme2008/defun
+    ;; NB: org.clojure/tools.macro is pulled in manually
+    [defun "0.3.0-RC1" :exclusions [org.clojure/tools.macro]] 
+    ;; More than one binding for if/when macros https://github.com/LockedOn/if-let
+    [lockedon/if-let "0.1.0"]
+    ;; Component Lifecycle https://github.com/stuartsierra/component
+    [com.stuartsierra/component "0.3.2"]
     [http-kit "2.3.0-alpha4"] ; HTTP client and server http://http-kit.org/
-    [ring/ring-codec "1.0.1"] ; Utility function for encoding and decoding data https://github.com/ring-clojure/ring-codec
-    [com.taoensso/timbre "4.10.0"] ; Pure Clojure/Script logging library https://github.com/ptaoussanis/timbre
-    [com.taoensso/sente "1.11.0"] ; WebSocket server https://github.com/ptaoussanis/sente
-    [com.taoensso/encore "2.92.0"] ; Force this version of encore, a dependency of Timbre and Sente
-    [raven-clj "1.5.0"] ; Interface to Sentry error reporting https://github.com/sethtrain/raven-clj
-    [liberator "0.15.1"] ; WebMachine (REST API server) port to Clojure https://github.com/clojure-liberator/liberator
-    [amazonica "0.3.112"] ; A comprehensive Clojure client for the AWS API. https://github.com/mcohen01/amazonica
-    [clj-jwt "0.1.1"] ; A Clojure library for JSON Web Token(JWT) https://github.com/liquidz/clj-jwt
-    [com.apa512/rethinkdb "0.15.26"] ; RethinkDB client for Clojure https://github.com/apa512/clj-rethinkdb
-    [com.taoensso/faraday "1.10.0-alpha1"] ; DynamoDB client https://github.com/ptaoussanis/faraday
-    [cheshire "5.8.0"] ; JSON encoding / decoding https://github.com/dakrone/cheshire
-    [clj-time "0.14.0"] ; Date and time lib https://github.com/clj-time/clj-time
-    [com.climate/squeedo "0.2.1"] ; AWS SQS consumer https://github.com/TheClimateCorporation/squeedo
-    [org.slf4j/slf4j-nop "1.8.0-alpha2"] ; Squeedo dependency
-    [prismatic/schema "1.1.6"] ; Data validation https://github.com/Prismatic/schema
-    [environ "1.1.0"] ; Environment settings from different sources https://github.com/weavejester/environ
-    [hickory "0.7.1"] ; HTML as data https://github.com/davidsantiago/hickory
+    ;; Utility function for encoding and decoding data https://github.com/ring-clojure/ring-codec
+    ;; NB: commons-codec gets picked up from amazonica
+    [ring/ring-codec "1.0.1" :exclusions [commons-codec]]
+    ;; Pure Clojure/Script logging library https://github.com/ptaoussanis/timbre
+    [com.taoensso/timbre "4.10.0"]
+    ;; Java logging lib https://commons.apache.org/proper/commons-logging/
+    ;; NB: Not used directly, but a very common dependency, so pulled in for manual version management
+    [commons-logging "1.2"]
+    ;; WebSocket server https://github.com/ptaoussanis/sente
+    [com.taoensso/sente "1.11.0"]
+    ;; Utility functions https://github.com/ptaoussanis/encore
+    ;; NB: Not used directly, forcing this version of encore, a dependency of Timbre and Sente
+    [com.taoensso/encore "2.92.0"]
+    ;; Interface to Sentry error reporting https://github.com/sethtrain/raven-clj
+    [raven-clj "1.5.0"]
+    ;; WebMachine (REST API server) port to Clojure https://github.com/clojure-liberator/liberator
+    [liberator "0.15.1"] 
+    ;; A comprehensive Clojure client for the AWS API. https://github.com/mcohen01/amazonica
+    ;; NB: joda-time is pulled in by clj-time
+    ;; NB: commons-logging is pulled in manually
+    ;; NB: com.fasterxml.jackson.core/jackson-databind is pulled in manually
+    [amazonica "0.3.112" :exclusions [joda-time commons-logging com.fasterxml.jackson.core/jackson-databind]]
+    ;; Data binding and tree for XML https://github.com/FasterXML/jackson-databind
+    ;; NB: Not used directly, but a very common dependency, so pulled in for manual version management
+    [com.fasterxml.jackson.core/jackson-databind "2.9.1"]
+    ;; A Clojure library for JSON Web Token(JWT) https://github.com/liquidz/clj-jwt
+    [clj-jwt "0.1.1"]
+    ;; RethinkDB client for Clojure https://github.com/apa512/clj-rethinkdb
+    [com.apa512/rethinkdb "0.15.26"]
+    ;; DynamoDB client https://github.com/ptaoussanis/faraday
+    ;; NB: com.amazonaws/aws-java-sdk-dynamodb is pulled in by amazonica
+    ;; NB: joda-time is pulled in by clj-time
+    [com.taoensso/faraday "1.10.0-alpha1" :exclusions [com.amazonaws/aws-java-sdk-dynamodb joda-time]]
+    ;; JSON encoding / decoding https://github.com/dakrone/cheshire
+    [cheshire "5.8.0"] 
+    ;; Date and time lib https://github.com/clj-time/clj-time
+    [clj-time "0.14.0"] 
+    ;; AWS SQS consumer https://github.com/TheClimateCorporation/squeedo
+    ;; NB: com.amazonaws/jmespath-java is pulled in by Amazonica
+    ;; NB: com.amazonaws/aws-java-sdk-sqs is pulled in by Amazonica
+    [com.climate/squeedo "0.2.1" :exclusions [com.amazonaws/jmespath-java com.amazonaws/aws-java-sdk-sqs]]
+    ;; Squeedo dependency
+    [org.slf4j/slf4j-nop "1.8.0-alpha2"]
+    ;; Data validation https://github.com/Prismatic/schema
+    [prismatic/schema "1.1.6"]
+    ;; Environment settings from different sources https://github.com/weavejester/environ
+    [environ "1.1.0"]
+    ;; HTML as data https://github.com/davidsantiago/hickory
+    [hickory "0.7.1" :exclusions [org.clojure/clojurescript]]
   ]
 
   :profiles {
@@ -44,27 +89,39 @@
     ;; QA environment and dependencies
     :qa {
       :dependencies [
-        [philoskim/debux "0.3.12"] ; `dbg` macro around -> or let https://github.com/philoskim/debux
-        [midje "1.9.0-alpha10"] ; Example-based testing https://github.com/marick/Midje
+        ;; Example-based testing https://github.com/marick/Midje
+        ;; NB: joda-time is pulled in by clj-time
+        ;; NB: org.clojure/tools.macro is pulled in manually
+        [midje "1.9.0-alpha10" :exclusions [joda-time org.clojure/tools.macro]] 
       ]
       :plugins [
-        [lein-midje "3.2.1"] ; Example-based testing https://github.com/marick/lein-midje
-        [jonase/eastwood "0.2.4"] ; Linter https://github.com/jonase/eastwood
-        [lein-kibit "0.1.6-beta2"] ; Static code search for non-idiomatic code https://github.com/jonase/kibit
+        ;; Example-based testing https://github.com/marick/lein-midje
+        [lein-midje "3.2.1"]
+        ;; Linter https://github.com/jonase/eastwood
+        [jonase/eastwood "0.2.4"]
+        ;; Static code search for non-idiomatic code https://github.com/jonase/kibit        
+        [lein-kibit "0.1.6-beta2" :exclusions [org.clojure/clojure]]
       ]
     }
 
     ;; Dev environment and dependencies
     :dev [:qa {
       :plugins [
-        [lein-bikeshed "0.4.1"] ; Check for code smells https://github.com/dakrone/lein-bikeshed
-        [lein-checkall "0.1.1"] ; Runs bikeshed, kibit and eastwood https://github.com/itang/lein-checkall
-        [lein-pprint "1.1.2"] ; pretty-print the lein project map https://github.com/technomancy/leiningen/tree/master/lein-pprint
-        [lein-ancient "0.6.12"] ; Check for outdated dependencies https://github.com/xsc/lein-ancient
-        [lein-spell "0.1.0"] ; Catch spelling mistakes in docs and docstrings https://github.com/cldwalker/lein-spell
-        [lein-deps-tree "0.1.2"] ; Print a tree of project dependencies https://github.com/the-kenny/lein-deps-tree
-        [venantius/yagni "0.1.4"] ; Dead code finder https://github.com/venantius/yagni
-        [com.jakemccrary/lein-test-refresh "0.21.1"] ; Autotest https://github.com/jakemcc/lein-test-refresh
+        ;; Check for code smells https://github.com/dakrone/lein-bikeshed
+        ;; NB: org.clojure/tools.cli is pulled in by lein-kibit
+        [lein-bikeshed "0.4.1" :exclusions [org.clojure/tools.cli]] 
+        ;; Runs bikeshed, kibit and eastwood https://github.com/itang/lein-checkall
+        [lein-checkall "0.1.1"]
+        ;; pretty-print the lein project map https://github.com/technomancy/leiningen/tree/master/lein-pprint
+        [lein-pprint "1.1.2"]
+        ;; Check for outdated dependencies https://github.com/xsc/lein-ancient
+        [lein-ancient "0.6.12"]
+        ;; Catch spelling mistakes in docs and docstrings https://github.com/cldwalker/lein-spell
+        [lein-spell "0.1.0"]
+        ;; Dead code finder https://github.com/venantius/yagni
+        [venantius/yagni "0.1.4" :exclusions [org.clojure/clojure]]
+        ;; Autotest https://github.com/jakemcc/lein-test-refresh
+        [com.jakemccrary/lein-test-refresh "0.21.1"]
       ]  
     }]
 
@@ -72,8 +129,10 @@
 
     :repl-config [:dev {
       :dependencies [
-        [org.clojure/tools.nrepl "0.2.13"] ; Network REPL https://github.com/clojure/tools.nrepl
-        [aprint "0.1.3"] ; Pretty printing in the REPL (aprint ...) https://github.com/razum2um/aprint
+        ;; Network REPL https://github.com/clojure/tools.nrepl
+        [org.clojure/tools.nrepl "0.2.13"]
+        ;; Pretty printing in the REPL (aprint ...) https://github.com/razum2um/aprint
+        [aprint "0.1.3"]
       ]
       ;; REPL injections
       :injections [

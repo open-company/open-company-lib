9eaf77bad137d47146083040d88205efa42a4ca9
bago2k4 <iacopo@rc0.eu>
2017-10-02 02:55:00 +0200

    Add linked and inline styles from the parsed sheet document to the returned HTML.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject open-company/lib "0.12.19"
+(defproject open-company/lib "0.12.21"
   :description "OpenCompany Common Library"
   :url "https://github.com/open-company/open-company-lib"
   :license {

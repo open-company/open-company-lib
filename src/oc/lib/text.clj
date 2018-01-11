62a52c6004b01d0c87d7bc51b05267090e3c9a84
Sean Johnson <sean@snootymonkey.com>
2018-01-11 08:08:10 -0500

    Add a text processing ns and an attribution function that builds human readable authorship attribution.

---
+++ src/oc/lib/text.clj
@@ -0,0 +1,35 @@
+(ns oc.lib.text
+  "Functions related to processing text.")
+
+(defn attribution
+  "
+  Given the number of distinct authors to mention, the number of items, what to call the
+  item (needs to pluralize with just an 's'), and a sequence of authors of the items
+  to attribute (sequence needs to be distinct'able, and have a `:name` property per author),
+  return a text string that attributes the authors to the items.
+
+  E.g.
+
+  (attribution 3 7 'comment' [{:name 'Joe'} {:name 'Joe'} {:name 'Moe'} {:name 'Flo'} {:name 'Flo'} {:name 'Sue'}])
+  '7 comments by Joe, Moe, Flo and others'
+  "
+  [attribution-count item-count item-name authors]
+  (let [distinct-authors (distinct authors)
+        author-names (map :name (take attribution-count distinct-authors))
+        more-authors? (> (count distinct-authors) (count author-names))
+        multiple-authors? (> (count author-names) 1)
+        author-attribution (cond
+                              ;; more distinct authors than we are going to mention
+                              more-authors?
+                              (str (clojure.string/join ", " author-names) " and others")
+                                                        
+                              ;; more than 1 author so last mention needs an "and", not a comma
+                              multiple-authors?
+                              (str (clojure.string/join ", " (butlast author-names))
+                                                        " and "
+                                                        (last author-names))
+
+                              ;; just 1 author
+                              :else
+                              (first author-names))]
+    (str item-count " " item-name (when (> item-count 1) "s") " by " author-attribution)))

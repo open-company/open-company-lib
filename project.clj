9c2e39bde3df0bdc29b472c4961e3d82a7fec769
Sean Johnson <sean@snootymonkey.com>
2018-02-16 09:48:10 -0500

    Fix return of echo/proxy so they return the initial message, not the reply. The timestamp of the return is stored by interaction services for thread lookup.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject open-company/lib "0.16.0"
+(defproject open-company/lib "0.16.1"
   :description "OpenCompany Common Library"
   :url "https://github.com/open-company/open-company-lib"
   :license {

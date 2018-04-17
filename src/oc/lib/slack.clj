7a91fbcdb0606de00cf3ede84137f7c7c143eccd
Nathan Zorn <nathan.zorn@gmail.com>
2018-04-17 15:55:59 -0400

    Support slack unfurl

--- src/oc/lib/slack.clj
+++ src/oc/lib/slack.clj
@@ -64,6 +64,13 @@
                                 :attachments (json/encode attachments)
                                 :channel channel}))
 
+(defn unfurl-post-url
+  "Add data to the url when a Carrot url is posted in slack."
+  [user-token channel url url-text]
+  (slack-api :chat.unfurl {:token user-token
+                           :channel channel
+                           :unfurls (json/encode {url {:text url-text}})}))
+
 (defn- slack-timestamp?
   "
   Slack timestamps look like: 1518175926.000301

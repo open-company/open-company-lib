ad8c1b56bc07170bf7286f1685ca7b46929d948b
bago2k4 <iacopo@rc0.eu>
2018-08-06 19:00:05 +0200

    Non alpha version: 0.16.12.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject open-company/lib "0.16.12-alpha3"
+(defproject open-company/lib "0.16.12"
   :description "OpenCompany Common Library"
   :url "https://github.com/open-company/open-company-lib"
   :license {

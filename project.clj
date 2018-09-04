eaba161973ac81a8c67f1bb29a0189913031fcec
Nathan Zorn <nathan.zorn@gmail.com>
2018-09-04 07:51:23 -0400

    drop the alpha

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject open-company/lib "0.16.14-alpha1"
+(defproject open-company/lib "0.16.14"
   :description "OpenCompany Common Library"
   :url "https://github.com/open-company/open-company-lib"
   :license {

15ae5fcf7303186ba3943d2fcc247165e1155958
bago2k4 <iacopo@rc0.eu>
2018-10-18 13:35:56 +0200

    Remove alpha0 suffix from version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject open-company/lib "0.16.16alpha0"
+(defproject open-company/lib "0.16.16"
   :description "OpenCompany Common Library"
   :url "https://github.com/open-company/open-company-lib"
   :license {

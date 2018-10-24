313a20886cc5449aaebe0065950bafe8c33302b3
bago2k4 <iacopo@rc0.eu>
2018-10-24 11:42:00 +0200

    Upgrade lib version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject open-company/lib "0.16.16"
+(defproject open-company/lib "0.16.17alpha"
   :description "OpenCompany Common Library"
   :url "https://github.com/open-company/open-company-lib"
   :license {

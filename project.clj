999a1bf22071541f3a0b149ac25cf3c327f4873d
Nathan Zorn <nathan.zorn@gmail.com>
2018-11-26 08:30:00 -0500

    version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject open-company/lib "0.16.22"
+(defproject open-company/lib "0.16.23alpha"
   :description "OpenCompany Common Library"
   :url "https://github.com/open-company/open-company-lib"
   :license {

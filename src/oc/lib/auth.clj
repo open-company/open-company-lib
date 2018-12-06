6c98d3b553fd05f312df4004e6f093596692aa76
bago2k4 <iacopo@rc0.eu>
2018-12-06 15:14:19 +0100

    Add an auth namespace needed to perform API calls with the auth service using a magic token.

---
+++ src/oc/lib/auth.clj
@@ -0,0 +1,47 @@
+(ns oc.lib.auth
+  "Uses a magic token to perform requests to the auth service like
+   getting a valid JWT or the complete user data."
+  (:require [cheshire.core :as json]
+            [org.httpkit.client :as http]
+            [clojure.walk :refer (keywordize-keys)]
+            [oc.lib.jwt :as jwt]))
+
+(defn- user-data [user-map]
+  (let [initial-map {:user user-map}
+        has-user-id (contains? user-map :user-id)
+        with-user-id (merge initial-map
+                       (when has-user-id
+                        {:user-id (:user-id user-map)}))
+        has-slack-user-id (contains? user-map :slack-user-id)
+        with-slack-user (merge with-user-id
+                         (when has-slack-user-id
+                           {:slack-user-id (:slack-user-id user-map)
+                            :slack-team-id (:slack-team-id user-map)}))]
+    with-slack-user))
+
+(defn- magic-token
+  [user-map passphrase service-name]
+  (jwt/generate (merge (user-data user-map)
+                 {:super-user true
+                  :name service-name
+                  :auth-source :services})
+    passphrase))
+
+(defn get-options
+  [token]
+  {:headers {"Content-Type" "application/vnd.open-company.auth.v1+json"
+             "Authorization" (str "Bearer " token)}})
+
+(defn user-token [user auth-server-url passphrase service-name]
+  (let [token-request
+        @(http/get (str auth-server-url "/users/refresh/")
+                   (get-options (magic-token user passphrase service-name)))]
+    (when (= 201 (:status token-request))
+      (:body token-request))))
+
+(defn user-data [user auth-server-url passphrase]
+  (let [user-request
+        @(http/get (str auth-server-url "/users/" (:user-id user))
+                   (get-options (magic-token user passphrase)))]
+    (when (= 200 (:status user-request))
+      (dissoc (keywordize-keys (json/parse-string (:body user-request))) :links))))

6f3e62ddff1dedb7648a32aae43c2bfb48fcb10b
bago2k4 <iacopo@rc0.eu>
2018-12-17 18:35:23 +0100

    Remove alpha.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject open-company/lib "0.16.29alpha6"
+(defproject open-company/lib "0.16.29"
   :description "OpenCompany Common Library"
   :url "https://github.com/open-company/open-company-lib"
   :license {

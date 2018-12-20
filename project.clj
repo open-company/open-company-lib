eb9485fb7202fecf856c04c45a99a44f1b9e36b2
bago2k4 <iacopo@rc0.eu>
2018-12-20 14:33:33 +0100

    Use the proper function to decode the id-token that supports old and new id tokens.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject open-company/lib "0.16.31alpha"
+(defproject open-company/lib "0.16.30.1alpha"
   :description "OpenCompany Common Library"
   :url "https://github.com/open-company/open-company-lib"
   :license {

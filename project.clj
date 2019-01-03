84d87bf2193db4bcddb1d2ac573ae05f65a52a49
Nathan Zorn <nathan.zorn@gmail.com>
2019-01-03 12:48:37 -0500

    return value of verify function

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject open-company/lib "0.16.31"
+(defproject open-company/lib "0.16.32alpha"
   :description "OpenCompany Common Library"
   :url "https://github.com/open-company/open-company-lib"
   :license {

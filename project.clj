9239100d645d9d19a0be515e0b81fea425b58148
Nathan Zorn <nathan.zorn@gmail.com>
2019-02-04 08:26:33 -0500

    version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject open-company/lib "0.16.36-alpha"
+(defproject open-company/lib "0.16.36"
   :description "OpenCompany Common Library"
   :url "https://github.com/open-company/open-company-lib"
   :license {

bb657a0b60a7092818136db7703579b95105f34a
bago2k4 <iacopo@rc0.eu>
2019-03-01 16:39:11 +0100

    Import the functions used to create the interaction copy for digest.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject open-company/lib "0.16.37"
+(defproject open-company/lib "0.16.38-alpha"
   :description "OpenCompany Common Library"
   :url "https://github.com/open-company/open-company-lib"
   :license {

6ed83aeff9a9e1237a21d7e2bddf11af1e324e99
bago2k4 <iacopo@rc0.eu>
2019-03-12 17:23:46 +0100

    Increase alpha version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject open-company/lib "0.16.39alpha-1"
+(defproject open-company/lib "0.16.39alpha-2"
   :description "OpenCompany Common Library"
   :url "https://github.com/open-company/open-company-lib"
   :license {

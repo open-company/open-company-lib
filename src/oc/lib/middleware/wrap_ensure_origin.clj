5485d824f3ad2cfba6e5f48aed9b1bd9e3e65262
bago2k4 <iacopo@rc0.eu>
2019-03-13 14:27:41 +0100

    Add try catch to avoid returning 500 if origin header is not present, return 403 if a NullPointerException is thrown.

--- src/oc/lib/middleware/wrap_ensure_origin.clj
+++ src/oc/lib/middleware/wrap_ensure_origin.clj
@@ -1,10 +1,17 @@
 (ns oc.lib.middleware.wrap-ensure-origin)
 
+(def origin-403-response
+ {:status 403
+  :body   "Forbidden: origin not allowed"})
+
 (defn wrap-ensure-origin
   [handler]
   (fn [request]
-    (let [origin-header (get-in request [:headers "origin"])]
-      (if (re-find #"(?i)^https:\/\/[staging\.|www\.]*carrot\.io\/?$" origin-header)
-        (handler request)
-        {:status 403
-         :body   "Forbidden: origin not allowed"}))))
+    (try
+     (let [origin-header (get-in request [:headers "origin"])]
+       (if (re-find #"(?i)^https:\/\/[staging\.|www\.]*carrot\.io\/?$" origin-header)
+         (handler request)
+         origin-403-response))
+     (catch java.lang.NullPointerException e
+       ;; Origin not provided
+       origin-403-response))))

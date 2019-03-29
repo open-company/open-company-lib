8267f803ed608af4de2e56cf749c06279ed7727c
bago2k4 <iacopo@rc0.eu>
2019-03-29 20:16:44 +0100

    Inc lib version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject open-company/lib "0.17.3-alpha4"
+(defproject open-company/lib "0.17.3-alpha7"
   :description "OpenCompany Common Library"
   :url "https://github.com/open-company/open-company-lib"
   :license {

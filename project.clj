e42414b04a65287bbec5b76ca1e0aad3ada4d13c
Sean Johnson <sean@snootymonkey.com>
2019-05-31 17:13:47 -0400

    Update lib version #.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject open-company/lib "0.17.11-alpha1"
+(defproject open-company/lib "0.17.11"
   :description "OpenCompany Common Library"
   :url "https://github.com/open-company/open-company-lib"
   :license {

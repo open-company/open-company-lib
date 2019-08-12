68974062af35cfa293b91ec17c778247bae6b452
Calvin Sauer <calvin.j.sauer@gmail.com>
2019-08-12 12:52:58 -0400

    Add parse-response fn for dealing with Lambda response payloads

--- src/oc/lib/lambda/common.clj
+++ src/oc/lib/lambda/common.clj
@@ -1,6 +1,14 @@
 (ns oc.lib.lambda.common
   (:require [amazonica.aws.lambda :as lambda]
-            [cheshire.core :as json]))
+            [cheshire.core :as json])
+  (:import [java.nio.charset StandardCharsets]))
+
+(defn parse-response
+  [{:keys [payload] :as response}]
+  (-> (.. StandardCharsets/UTF_8 (decode payload) toString)
+      (json/parse-string keyword)
+      :body
+      (json/parse-string keyword)))
 
 (defn invoke-fn
   [fn-name payload]

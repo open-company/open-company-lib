572403822f4ad38a5f4afdb04c130a8c55f11f6a
Calvin Sauer <calvin.j.sauer@gmail.com>
2019-08-26 11:29:45 -0400

    Add doc notes to lambda.common ns

--- src/oc/lib/lambda/common.clj
+++ src/oc/lib/lambda/common.clj
@@ -1,9 +1,11 @@
 (ns oc.lib.lambda.common
+  "Utility functions for invoking AWS Lambda functions."
   (:require [amazonica.aws.lambda :as lambda]
             [cheshire.core :as json])
   (:import [java.nio.charset StandardCharsets]))
 
 (defn parse-response
+  "Parses response returned by call to `invoke-fn` back into clj data."
   [{:keys [payload] :as response}]
   (-> (.. StandardCharsets/UTF_8 (decode payload) toString)
       (json/parse-string keyword)
@@ -11,6 +13,8 @@
       (json/parse-string keyword)))
 
 (defn invoke-fn
+  "Invokes AWS lambda function with given fn-name and payload.
+  payload should be clj data."
   [fn-name payload]
   (let [json-payload (json/generate-string payload)]
     (lambda/invoke :function-name fn-name

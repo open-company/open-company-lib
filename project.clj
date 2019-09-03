a5914e348628ab1dfe1cbc110289ef0f7590efcd
Calvin Sauer <calvin.j.sauer@gmail.com>
2019-09-03 14:16:19 -0400

    alpha1 instead of SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject open-company/lib "0.17.16-SNAPSHOT"
+(defproject open-company/lib "0.17.16-alpha1"
   :description "OpenCompany Common Library"
   :url "https://github.com/open-company/open-company-lib"
   :license {

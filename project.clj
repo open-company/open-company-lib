73f4c507cac12837e1dba4082d161a04a6bfe332
Calvin Sauer <calvin.j.sauer@gmail.com>
2019-09-10 17:38:29 -0400

    Bump to non-alpha version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject open-company/lib "0.17.17-alpha"
+(defproject open-company/lib "0.17.17"
   :description "OpenCompany Common Library"
   :url "https://github.com/open-company/open-company-lib"
   :license {

e6cda8f13f68f48ba2eec0308e09270cd4cd6d73
bago2k4 <iacopo@rc0.eu>
2019-11-11 16:00:39 +0100

    Remove alpha from lib 0.17.19.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject open-company/lib "0.17.19-alpha9"
+(defproject open-company/lib "0.17.19"
   :description "OpenCompany Common Library"
   :url "https://github.com/open-company/open-company-lib"
   :license {

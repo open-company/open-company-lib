a04fe9a55d25b78d31f46940dec16c114e41f33c
bago2k4 <iacopo@rc0.eu>
2020-03-09 22:27:50 +0100

    Bump lib version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject open-company/lib "0.17.25.3-alpha"
+(defproject open-company/lib "0.17.25.3-alpha1"
   :description "OpenCompany Common Library"
   :url "https://github.com/open-company/open-company-lib"
   :license {

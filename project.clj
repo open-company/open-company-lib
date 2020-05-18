60996c925888903194cab13bfe281d3a71418d59
bago2k4 <iacopo@rc0.eu>
2020-05-18 22:22:30 +0200

    Pass the origin of the request to the wrap-ensure-origin function to check it.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject open-company/lib "0.17.29-alpha13"
+(defproject open-company/lib "0.17.29-alpha14"
   :description "OpenCompany Common Library"
   :url "https://github.com/open-company/open-company-lib"
   :license {
--- src/oc/lib/middleware/wrap_ensure_origin.clj
+++ src/oc/lib/middleware/wrap_ensure_origin.clj
@@ -1,19 +1,22 @@
-(ns oc.lib.middleware.wrap-ensure-origin)
+(ns oc.lib.middleware.wrap-ensure-origin
+  (:require [clojure.string :as cstr]))
 
 (def origin-403-response
  {:status 403
   :body   "Forbidden: origin not allowed"})
 
 (defn wrap-ensure-origin
-  [handler]
-  (fn [request]
-    (try
-     (let [websocket? (:websocket? request)
-           origin-header (get-in request [:headers "origin"])]
-       (if (or (not websocket?) ; we only check origin on websocket requests
-                (re-find #"(?i)^https:\/\/[staging\.|www\.]*[carrot\.io|wuts\.io]\/?$" origin-header))
-         (handler request) ; all is well
-         origin-403-response)) ; ye shall not pass
-     (catch java.lang.NullPointerException e
-       ;; Origin not provided, also a fail
-       origin-403-response))))
+  [handler origin-domain]
+  (let [escaped-origin-domain (cstr/replace origin-domain #"(\.|/)" "\\\\$1")
+        origin-re (re-pattern (str "(?i)^" escaped-origin-domain "\\/?$"))]
+    (fn [request]
+      (try
+       (let [websocket? (:websocket? request)
+             origin-header (get-in request [:headers "origin"])]
+         (if (or (not websocket?) ; we only check origin on websocket requests
+                 (re-find origin-re origin-header))
+           (handler request) ; all is well
+           origin-403-response)) ; ye shall not pass
+       (catch java.lang.NullPointerException e
+         ;; Origin not provided, also a fail
+         origin-403-response)))))

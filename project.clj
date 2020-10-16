7b2b9a445ba0dc334eab7741ca8ecbb82e476f1b
bago2k4 <iacopo@rc0.eu>
2020-10-16 18:27:32 +0200

    Add interval and useragent cljs namespaces needed by web and desktop clients and bump version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject open-company/lib "0.17.29-alpha49"
+(defproject open-company/lib "0.17.29-alpha50"
   :description "OpenCompany Common Library"
   :url "https://github.com/open-company/open-company-lib"
   :license {

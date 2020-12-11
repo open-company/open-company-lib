7dbcb3226c319f07baa95165f75ed0fd4083d2b3
bago2k4 <iacopo@rc0.eu>
2020-12-11 21:46:49 +0100

    Make sure all resource templates have the handle-exception handler set.

--- src/oc/lib/api/common.clj
+++ src/oc/lib/api/common.clj
@@ -277,12 +277,14 @@
   :initialize-context (fn [ctx] (read-token (get-in ctx [:request :headers]) passphrase))
   :authorized? allow-anonymous
   :handle-unauthorized handle-unauthorized
+  :handle-exception handle-exception
   :handle-forbidden  (fn [ctx] (if (:jwtoken ctx) (forbidden-response) (unauthorized-response)))})
 
 (defn base-authenticated-resource [passphrase]{
   :initialize-context (fn [ctx] (read-token (get-in ctx [:request :headers]) passphrase))
   :handle-not-found (fn [_] (missing-response))
   :handle-unauthorized handle-unauthorized
+  :handle-exception handle-exception
   :handle-forbidden (fn [_] (forbidden-response))})
 
 (defn id-token-resource [passphrase]
